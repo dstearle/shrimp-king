d2d148feb006d9f351621d877ae1c67b917070dc
dstearle <dallas.st.earle@gmail.com>
2019-08-10 16:13:34 -0700

    Added Vuex package and began work on store

---
+++ src/data/stocks.js
@@ -0,0 +1,9 @@
+export default [
+    
+    { id: 1, name: 'Cherry Shrimp', price: 110 },
+                    
+    { id: 2, name: 'Lemon Shrimp', price: 200 },
+                    
+    { id: 3, name: 'Blueberry Shrimp', price: 250 },
+        
+];
