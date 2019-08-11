680f3019ef989b3672e24e652ee152b3b8cd7bad
dstearle <dallas.st.earle@gmail.com>
2019-08-11 00:53:49 -0700

    Gave proper images to each shrimp

--- src/data/shrimpData.js
+++ src/data/shrimpData.js
@@ -4,9 +4,9 @@ export default [
     { id: 1, name: 'Cherry Shrimp', price: 110, photo: require("./../assets/images/cherryShrimp.png") },
      
     // Lemon Shrimp
-    { id: 2, name: 'Lemon Shrimp', price: 200, photo: require("./../assets/images/cherryShrimp.png") },
+    { id: 2, name: 'Lemon Shrimp', price: 200, photo: require("./../assets/images/lemonShrimp.jpg") },
     
     // Blueberry Shrimp
-    { id: 3, name: 'Blueberry Shrimp', price: 250, photo: require("./../assets/images/cherryShrimp.png") },
+    { id: 3, name: 'Blueberry Shrimp', price: 250, photo: require("./../assets/images/blueberryShrimp.jpg") },
         
 ];
