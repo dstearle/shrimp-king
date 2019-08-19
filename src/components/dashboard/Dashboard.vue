e37d7669c3a2b5381c16fc1213c462a0c34a9b1d
dstearle <dallas.st.earle@gmail.com>
2019-08-19 03:24:51 -0700

    Created new folder for dashboard and made component for SaveLoad

---
+++ src/components/dashboard/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+    
+    <div class="container">
+
+        <!-- Save & Load Buttons -->
+        <app-save-load></app-save-load>
+        
+        <div class="row py-3">
+
+            <div class="col">
+
+                <div class="card">
+
+                    <div class="card-body">
+
+                        <h1>Shrimp King Dashboard</h1>
+            
+                        <h6>You may Save & Load your Data</h6>
+                        
+                        <h6>Click on 'End Day' to begin a New Day!</h6>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        
+    </div>
+    
+</template>
+
+<script>
+
+    import SaveLoad from "./SaveLoad.vue"
+
+    export default {
+
+        components: {
+            
+            appSaveLoad: SaveLoad
+            
+        },
+        
+    }
+
+</script>
