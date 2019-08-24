c2776a4a7d9b3c3bc8e328abb42a53511d9928d8
dstearle <dallas.st.earle@gmail.com>
2019-08-24 07:07:10 -0700

    Added alt descriptions for shrimp images

--- src/store/modules/shrimpInventory.js
+++ src/store/modules/shrimpInventory.js
@@ -110,6 +110,8 @@ const getters = {
 
                 photo: record.photo,
 
+                alt: record.alt,
+
                 overlayText: record.overlayText,
 
                 text: record.text,
