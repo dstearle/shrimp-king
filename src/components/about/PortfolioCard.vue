e1451df3c54fcbc845fcda806373a24402f3e6a6
dstearle <dallas.st.earle@gmail.com>
2019-09-26 13:47:15 -0700

    Refactored PortfolioCard into a new component

---
+++ src/components/about/PortfolioCard.vue
@@ -0,0 +1,50 @@
+<template>
+    
+    <div>
+
+        <!-- Links To Portfolio & Github -->
+        <div class="card bg-success text-white">
+
+            <div class="card-body">
+
+                <div class="row">
+
+                    <div class="col-4">hi</div>
+
+                    <div class="col-8">
+
+                        <p>
+                            Interested in seeing or learning more about Shrimp King or other projects I have worked on? 
+                            This project along with many others can be found on my personal portfolio and github profile!
+                        </p>
+
+                        <!-- Card Footer -->
+                        <div class="row" style="padding:0px 20px 0px 20px">
+
+                            <strong class="mr-auto">
+
+                                <!-- My Portfolio -->
+                                <a class="text-white" href="http://dstearle.com/">my portfolio</a>
+
+                            </strong>
+
+                            <strong class="ml-auto">
+
+                                <!-- Github Repo -->
+                                <a class="text-white" href="https://github.com/dstearle">github profile</a>
+
+                            </strong>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
