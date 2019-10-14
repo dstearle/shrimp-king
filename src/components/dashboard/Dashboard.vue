3615a73f3e2a0617de8a0bcf8cb7265c6270c9aa
dstearle <dallas.st.earle@gmail.com>
2019-10-14 00:38:43 -0700

    Began working on new component for showing favorited items on the Dashboard

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -5,6 +5,7 @@
         <!-- Save & Load Buttons -->
         <app-save-load></app-save-load>
         
+        <!-- Dashboard Chart -->
         <div class="row py-3">
 
             <div class="col">
@@ -22,6 +23,25 @@
             </div>
 
         </div>
+
+        <!-- Chart Favorites -->
+        <div class="row py-3">
+
+            <div class="col">
+
+                <div class="card">
+
+                    <div class="card-body px-5 pb-5">
+
+                        <app-chart-favorites></app-chart-favorites>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
         
     </div>
     
@@ -31,13 +51,15 @@
 
     import SaveLoad from "./SaveLoad.vue"
     import DashboardChart from "./DashboardChart.vue"
+    import ChartFavorites from "./ChartFavorites.vue"
 
     export default {
 
         components: {
             
             appSaveLoad: SaveLoad,
-            appDashboardChart: DashboardChart
+            appDashboardChart: DashboardChart,
+            appChartFavorites: ChartFavorites,
             
         },
         
