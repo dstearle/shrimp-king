e1cb466b70b119f4a5282bcf9d8accc301891028
dstearle <dallas.st.earle@gmail.com>
2019-10-21 00:22:18 -0700

    Created new component for empty slots on the dashboard

---
+++ src/components/dashboard/dbFavorites/EmptySlots.vue
@@ -0,0 +1,124 @@
+<template>
+    
+    <div class="row mb-3 text-secondary">
+
+        <!-- Empty Photo --> 
+        <div class="mr-4 empty-photo">
+
+
+        </div>
+
+        <div>
+
+            <!-- Title -->
+            <div class="row m-0">
+
+                <h6 class="align-self-center m-0">
+
+                    <u>Empty Slot</u>
+
+                </h6>
+
+            </div>
+
+            <!-- Remove From Favorites -->
+            <div 
+                @click="favoriteItem"
+                @mouseover="activeStar = true"
+                @mouseleave="activeStar = false"
+                class="row pt-1 m-0"
+            >
+
+                <!-- Star Icon -->
+                <font-awesome-icon 
+                    icon="star"
+                    class="align-self-center m-0"
+                    :class="{ 'text-secondary inActiveHover' : !activeStar , 'text-warning': activeStar }"
+                />
+
+                <!-- Remove Text -->
+                <p class="align-self-center m-0"> Add</p>
+
+            </div>
+
+        </div>
+        
+    </div>
+    
+</template>
+
+<script>
+
+    import {mapGetters, mapActions} from 'vuex';
+
+    export default {
+        
+        props: ['shrimp'],
+
+        data() {
+            
+            return {
+
+                activeStar: false,
+                
+            }
+            
+        },
+        
+        computed: {
+
+            ...mapGetters({
+                
+                // Retrieves the counter for favorited items
+                favoritesCounterGet: 'favoritesCounterGet'
+                
+            }), 
+            
+        },
+        
+        methods: {
+
+            ...mapActions({
+
+                favUp: 'favUp',
+                favDown: 'favDown'
+
+            }),
+
+            // Method for favoriting an item
+            favoriteItem() {
+
+                // Toggles the item from true to false (or vice versa)
+                this.shrimp.favorited = !this.shrimp.favorited;
+                
+                // Check to see if item is favorited and adds to the counter
+                if(this.shrimp.favorited) {this.favUp();}
+                // Detracts from the counter if unfavorited
+                else {this.favDown();}
+
+            },
+
+            // Method for icon hover toggles
+            mouseOver() {
+
+                // Toggles star classes
+                this.activeStar = !this.activeStar;
+
+            },
+            
+        }
+        
+    }
+
+</script>
+
+<style scoped>
+
+    .empty-photo {
+        background: lightgray;
+        height: 4rem;
+        width: 4rem;
+        border-radius: 9999px;
+    }
+
+</style>
