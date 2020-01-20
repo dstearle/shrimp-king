58926796d8c0d4dc801ec3bee0345c104ab727d1
dstearle <dallas.st.earle@gmail.com>
2020-01-19 22:12:30 -0800

    Imported in basic logic for SearchFilter

--- src/components/searchFilter/SearchFilter.vue
+++ src/components/searchFilter/SearchFilter.vue
@@ -4,13 +4,12 @@
 
         <div class="col form-group">
 
-            <!-- Search Filter Input -->
-            <input 
-                type="text" 
-                name="searchFilter" 
-                placeholder="Search by #Tag..."
-                class="col form-control form-control-lg" 
-            />
+            <!-- Search Select Input -->
+            <search-select 
+                v-model="selectedEmbryo" 
+                :options="embryos"
+                :filter-function="applySearchFilter"
+            ></search-select>
 
         </div>
 
@@ -19,6 +18,59 @@
 </template>
 
 <script>
+
+    import SearchSelect from './SearchSelect.vue'
+
+    export default {
+
+        components: {
+
+            SearchSelect
+
+        },
+
+        data() {
+
+            return {
+
+                selectedEmbryo: null,
+                embryos: [
+
+                    "Apatosaurus",
+                    "Suchomimus",
+                    "Dryosaurus",
+                    "Kentrosaurus",
+                    "Velociraptor",
+                    "Pachyrhinosaurus",
+                    "Brachiosaurus",
+                    "Iguanodon",
+                    "T-Rex",
+                    "Proceratosaurus",
+                    "Dracorex",
+                    "Carnotaurus",
+                    "Mamenchisaurus"
+
+                ]
+
+            }
+
+        },
+
+        methods: {
+
+            // Filters the list with user input
+            applySearchFilter(search, options) {
+
+                return options.filter(option => {
+                return option.toLowerCase().startsWith(search.toLowerCase())
+
+                })
+
+            }
+
+        },
+
+    }
 
 </script>
 
