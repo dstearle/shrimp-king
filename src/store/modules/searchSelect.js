87a1e244f1f316df2e5427fad18c28d14560c28f
dstearle <dallas.st.earle@gmail.com>
2020-01-29 02:30:05 -0800

    Resorted to using mapMutation instead of mapAction for sending data to store

--- src/store/modules/searchSelect.js
+++ src/store/modules/searchSelect.js
@@ -1,24 +1,51 @@
 const state = {
 
     // Array to store available shrimp for the Market
-    selectedTags: []
+    selectedTags2: []
     
 };
 
+const mutations = {
+    
+    // Sets the stored data from imports into the array
+    'ADD_TAG' (state, tag) {
+        
+        state.selectedTags2.push(tag);
+        
+    }, 
+    
+};
 
-// const actions = {
+const actions = {
     
-//     // 
+    // Adds a new tag to the state
+    addTag: ({commit, tag}) => {
+        
+        commit('ADD_TAG', tag);
+        
+    }, 
     
-// };
+};
 
 const getters = {
     
     // Getter for available shrimp in Market
-    selectedTagsGet: state => {
+    selectedTags2Get: state => {
         
-        return state.selectedTags;
+        return state.selectedTags2;
         
     }
     
 };
+
+export default {
+    
+    state,
+
+    mutations,
+    
+    actions,
+    
+    getters
+    
+}
